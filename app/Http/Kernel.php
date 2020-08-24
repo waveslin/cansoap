8b0795329928a6488ab2621293622badbbacd6c2
WAVES-STUDIO <35038769+WAVES-STUDIO@users.noreply.github.com>
2020-08-23 22:40:51 -0400

    '18th'

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HttpsProtocol::class
         ],
 
         'api' => [
@@ -64,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'check.cart' => \App\Http\Middleware\CheckCart::class
+        'check.cart' => \App\Http\Middleware\CheckCart::class,
+        'forcessl' => \App\Http\Middleware\CheckCart::class
     ];
 }
