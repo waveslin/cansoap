f65770c66794e57ac7adae3e89b67061fef2b219
WAVES-STUDIO <35038769+WAVES-STUDIO@users.noreply.github.com>
2020-08-23 22:03:06 -0400

    '9th'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
         if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+        if (app()->environment('remote')) {
+            URL::forceScheme('https');
+        }
     }
 }
