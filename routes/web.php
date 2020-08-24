f1eb3f7f58ede0bae88dc4559c7f8f148eaf38a9
WAVES-STUDIO <35038769+WAVES-STUDIO@users.noreply.github.com>
2020-08-23 22:42:18 -0400

    '19th'

--- routes/web.php
+++ routes/web.php
@@ -13,32 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', ['uses' => 'ShopController@showIndex', 'as' => 'soap.index'])->middleware('forcessl');
+Route::get('/', ['uses' => 'ShopController@showIndex', 'as' => 'soap.index']);
 
-Route::get('about', ['uses' => 'ShopController@showAbout', 'as' => 'soap.about'])->middleware('forcessl');
+Route::get('about', ['uses' => 'ShopController@showAbout', 'as' => 'soap.about']);
 
-Route::get('contact', ['uses' => 'ShopController@showContact', 'as' => 'soap.contact'])->middleware('forcessl');
+Route::get('contact', ['uses' => 'ShopController@showContact', 'as' => 'soap.contact']);
 
-Route::get('order', ['uses' => 'ShopController@showOrder', 'as' => 'soap.order'])->middleware('forcessl');
+Route::get('order', ['uses' => 'ShopController@showOrder', 'as' => 'soap.order']);
 
-Route::get('privacy', ['uses' => 'ShopController@showPrivacy', 'as' => 'soap.privacy'])->middleware('forcessl');
+Route::get('privacy', ['uses' => 'ShopController@showPrivacy', 'as' => 'soap.privacy']);
 
-Route::get('wholesale', ['uses' => 'ShopController@showWholesale', 'as' => 'soap.wholesale'])->middleware('forcessl');
+Route::get('wholesale', ['uses' => 'ShopController@showWholesale', 'as' => 'soap.wholesale']);
 
-Route::get('collection', ['uses' => 'ShopController@showCollection', 'as' => 'soap.collection'])->middleware('forcessl');
+Route::get('collection', ['uses' => 'ShopController@showCollection', 'as' => 'soap.collection']);
 
-Route::get('product/{product_name}', ['uses' => 'ShopController@showProduct', 'as' => 'soap.product'])->middleware('forcessl');
+Route::get('product/{product_name}', ['uses' => 'ShopController@showProduct', 'as' => 'soap.product']);
 
-Route::post('product/{product_name}', ['uses' => 'ShopController@addProduct', 'as' => 'soap.add'])->middleware('forcessl');
+Route::post('product/{product_name}', ['uses' => 'ShopController@addProduct', 'as' => 'soap.add']);
 
-Route::get('cart', ['uses' => 'ShopController@showCart', 'as' => 'soap.cart'])->middleware('forcessl');
+Route::get('cart', ['uses' => 'ShopController@showCart', 'as' => 'soap.cart']);
  
-Route::post('cart', ['uses' => 'ShopController@updateCart', 'as' => 'soap.update'])->middleware('forcessl');
+Route::post('cart', ['uses' => 'ShopController@updateCart', 'as' => 'soap.update']);
 
-Route::get('remove/{product_name}', ['uses' => 'ShopController@removeCart', 'as' => 'soap.remove'])->middleware('forcessl');
+Route::get('remove/{product_name}', ['uses' => 'ShopController@removeCart', 'as' => 'soap.remove']);
 
-Route::get('payment', ['uses' => 'ShopController@getPayment', 'as' => 'soap.checkout'])->middleware('check.cart', 'forcessl');
+Route::get('payment', ['uses' => 'ShopController@getPayment', 'as' => 'soap.checkout', 'middleware' => 'check.cart']);
 
-Route::post('payment', ['uses' => 'ShopController@makePayment', 'as' => 'soap.payment'])->middleware('check.cart', 'forcessl');;
+Route::post('payment', ['uses' => 'ShopController@makePayment', 'as' => 'soap.payment', 'middleware' => 'check.cart']);
 
-Route::get('receipt/{oid}', ['uses' => 'pdfController@makeReceipt', 'as' => 'soap.receipt'])->middleware('forcessl');
+Route::get('receipt/{oid}', ['uses' => 'pdfController@makeReceipt', 'as' => 'soap.receipt']);
